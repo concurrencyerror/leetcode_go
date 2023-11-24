87a21bfee1bb3d5531b8fa40a01659f495451de0
Horace <horce1024@gmail.com>
2023-11-24 09:12:56 +0800

    每日一题

---
+++ solution2824.go
@@ -0,0 +1,13 @@
+package leetcode_go
+
+func countPairs(nums []int, target int) int {
+	result := 0
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] < target {
+				result++
+			}
+		}
+	}
+	return result
+}
